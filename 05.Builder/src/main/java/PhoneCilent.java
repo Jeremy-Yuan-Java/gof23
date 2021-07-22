ec395d010695c34afe410f2c77d58bb425c663d4
yuanmeijie <jeremt_yuan@126.com>
2021-07-22 09:45:05 +0800

    feat:构造者设计模式学习

---
+++ 05.Builder/src/main/java/PhoneCilent.java
@@ -0,0 +1,71 @@
+class PhoneClient {
+    public static void main(String[] args) {
+        Phone phone = new Phone.Builder()
+                .cpu("intel")
+                .mainboard("华硕")
+                .memory("金士顿")
+                .screen("三星")
+                .build();
+        System.out.println(phone);
+    }
+}
+
+class Phone {
+
+    private String cpu;
+    private String screen;
+    private String memory;
+    private String mainboard;
+
+    private Phone(Builder builder) {
+        cpu = builder.cpu;
+        screen = builder.screen;
+        memory = builder.memory;
+        mainboard = builder.mainboard;
+    }
+
+    public static final class Builder {
+        private String cpu;
+        private String screen;
+        private String memory;
+        private String mainboard;
+
+        public Builder() {
+        }
+
+        public Builder cpu(String val) {
+            cpu = val;
+            return this;
+        }
+
+        public Builder screen(String val) {
+            screen = val;
+            return this;
+        }
+
+        public Builder memory(String val) {
+            memory = val;
+            return this;
+        }
+
+        public Builder mainboard(String val) {
+            mainboard = val;
+            return this;
+        }
+
+        public Phone build() {
+            return new Phone(this);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Phone{" +
+                "cpu='" + cpu + '\'' +
+                ", screen='" + screen + '\'' +
+                ", memory='" + memory + '\'' +
+                ", mainboard='" + mainboard + '\'' +
+                '}';
+    }
+}
+
